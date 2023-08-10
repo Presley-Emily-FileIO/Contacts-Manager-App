f531991fadf515eb1998c357602aaa5abec2a9e7
Presley Black <presleyblack24@gmail.com>
2023-08-10 13:36:35 -0500

    update

--- src/Contact.java
+++ src/Contact.java
@@ -3,16 +3,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Contact {
     private String name;
 
     private long phoneNumber;
+    ArrayList<String>contacts;
 
     // constructors ---
     public Contact(String name, long phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
+        this.contacts = new ArrayList<>();
     }
 
     //    Getters and setters--
