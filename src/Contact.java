b8356a7a25d44d2d282a4a1472416205a6d92972
Presley Black <presleyblack24@gmail.com>
2023-08-10 11:39:32 -0500

    begining application

--- src/Contact.java
+++ src/Contact.java
@@ -1,5 +1,38 @@
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Contact {
     private String name;
 
+    private long phoneNumber;
+
+    // constructors ---
+    public Contact(String name, long phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //    Getters and setters--
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
 
 }
+
---
+++ src/ContactManagerApplication.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ContactManagerApplication {
+    public void createDirectoryFile(){
+        String directory = "Contacts";
+        String fileName = "contacts.txt";
+        Path contactsDirectory = Paths.get(directory);
+        Path contactsFile = Paths.get(fileName);
+        if (!Files.exists(contactsDirectory)){
+            try {
+                Files.createDirectories(contactsDirectory);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        if (!Files.exists(contactsFile)){
+            try {
+                Files.createFile(contactsFile);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+
+}
